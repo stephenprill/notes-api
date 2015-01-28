f046e495336443cfcb277a81c670b8331076f251
Stephen Prill <stephen.prill@gmail.com>
2015-01-28 10:20:21 -0700

    added create, show, update, destroy to the notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,18 +4,23 @@ class NotesController < ApplicationController
     @notes = Note.all
     render json: @notes
   end
-
+  
   def create
-
+    @note = Note.new(params.require(:note).permit(:title, :body))
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def update
+    @note = Note.find(params[:id])
+    @note.update(params.require(:note).permit(:title, :body))
   end
 
   def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
   end
 
 
